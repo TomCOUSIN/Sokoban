846ce668bc7847531e789bd36168e8c12a08f523
tomcousin <tom.cousin@epitech.eu>
2021-02-02 22:19:28 +0900

    [~] Initialize Project

---
+++ Sokoban/Sokoban/SokobanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SokobanApp.swift
+//  Sokoban
+//
+//  Created by Tom Cousin on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SokobanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
