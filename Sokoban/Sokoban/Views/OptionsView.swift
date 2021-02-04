5df8d5ea7c172781813858400e49e392fa12547b
tomcousin <tom.cousin@epitech.eu>
2021-02-04 23:09:56 +0900

    [+] Add OptionsView to manage options

---
+++ Sokoban/Sokoban/Views/OptionsView.swift
@@ -0,0 +1,28 @@
+//
+//  OptionsView.swift
+//  Sokoban
+//
+//  Created by Tom Cousin on 04/02/2021.
+//
+
+import SwiftUI
+
+struct OptionsView: View {
+    
+    // MARK: - Body
+    
+    var body: some View {
+        HStack {
+            CircleButton(name: "arrow.counterclockwise", action: {}, width: 50, height: 50)
+            Spacer()
+        }
+    }
+}
+
+struct OptionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        OptionsView()
+        OptionsView()
+            .preferredColorScheme(.dark)
+    }
+}
