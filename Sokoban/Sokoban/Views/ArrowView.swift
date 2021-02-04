7cdea4c887a985c0863088f9bd6ce6e852a59503
tomcousin <tom.cousin@epitech.eu>
2021-02-04 22:39:48 +0900

    [+] Add The ArrowView to display Arrows

---
+++ Sokoban/Sokoban/Views/ArrowView.swift
@@ -0,0 +1,30 @@
+//
+//  ArrowView.swift
+//  Sokoban
+//
+//  Created by Tom Cousin on 04/02/2021.
+//
+
+import SwiftUI
+
+struct ArrowView: View {
+    
+    // MARK: - Body
+    
+    var body: some View {
+        VStack(spacing: -10) {
+            CircleButton(name: "arrow.up", action: {})
+            HStack(spacing: 40) {
+                CircleButton(name: "arrow.left", action: {})
+                CircleButton(name: "arrow.right", action: {})
+            }
+            CircleButton(name: "arrow.down", action: {})
+        }
+    }
+}
+
+struct ArrowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArrowView()
+    }
+}
